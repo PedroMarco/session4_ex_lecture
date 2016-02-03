661b264464e4f3b229d8ec051c075eb19a7ebb5f
pedromarco <pedrom28@gmail.com>
2016-02-03 20:36:36 +0000

    OnlyComments

--- MonteCarloSimulation.py
+++ MonteCarloSimulation.py
@@ -1,17 +1,20 @@
+## NOTE: Area of the square, 2x2 = 4. Area of the circle (radius 1) pi*r^2, so pi, so 3.141592
+##
+
 import random
 
 NUMBER_OF_TRIALS = 100000 # Constant
 numberOfHits = 0
 
 for i in range(NUMBER_OF_TRIALS):
-    x = random.random() * 2 - 1
-    y = random.random() * 2 - 1
+    x = random.random() * 2 - 1   ##  random.random() Return the next random floating point number in the range [0.0, 1.0)
+    y = random.random() * 2 - 1   ## *2 -1 will make value possible from 1 to -1 so axis can be generated
 
-    if x * x + y * y <= 1:
+    if x * x + y * y <= 1:  ##  Why does this formula means that we enter the pi area?
         numberOfHits += 1
 
 pi = 4 * numberOfHits / NUMBER_OF_TRIALS
 
 print("PI is", pi)
 
-### 4* ve
+###### Coordinates give the radius size, and could be up to 2 even being inside the square? Don't think so.
